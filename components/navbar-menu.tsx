532c2629452df9547c17954fb9253fd06a57757f
andriinakonechnyi.work <andriinakonechnyi.work@gmail.com>
2026-06-28 17:09:25 +0300

    feat: change nav menu

--- components/navbar-menu.tsx
+++ components/navbar-menu.tsx
@@ -1,46 +1,50 @@
 import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu";
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
 
 export default function NavbarMenu() {
   const t = useTranslations("navbar");
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem className="relative">
-          <NavigationMenuTrigger className="text-primary">
-            {t("portfolios")}
-          </NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink asChild>
-              <Link href="/portfolio">{t("dashboard")}</Link>
-            </NavigationMenuLink>
-            <NavigationMenuLink asChild>
-              <Link href="/portfolio/trades">{t("trades")}</Link>
-            </NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem className="relative">
-          <NavigationMenuTrigger className="text-primary">
-            {t("net-worth")}
-          </NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink asChild>
-              <Link href="/net-worth/dashboard">Dashboard</Link>
-            </NavigationMenuLink>
-            <NavigationMenuLink asChild>
-              <Link href="/net-worth/budget">{t("budget")}</Link>
-            </NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <Menubar className="border-none bg-transparent">
+      {/* Portfolios */}
+      <MenubarMenu>
+        <MenubarTrigger className="text-primary cursor-pointer">
+          {t("portfolios")}
+        </MenubarTrigger>
+
+        <MenubarContent>
+          <MenubarItem asChild>
+            <Link href="/portfolio">{t("dashboard")}</Link>
+          </MenubarItem>
+
+          <MenubarItem asChild>
+            <Link href="/portfolio/trades">{t("trades")}</Link>
+          </MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+
+      {/* Net Worth */}
+      <MenubarMenu>
+        <MenubarTrigger className="text-primary cursor-pointer">
+          {t("net-worth")}
+        </MenubarTrigger>
+
+        <MenubarContent>
+          <MenubarItem asChild>
+            <Link href="/net-worth/dashboard">Dashboard</Link>
+          </MenubarItem>
+
+          <MenubarItem asChild>
+            <Link href="/net-worth/budget">{t("budget")}</Link>
+          </MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
   );
 }
